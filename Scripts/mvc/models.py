5e474790b02367344315112bb1aca059a8835775
andreramosneves <andreramosneves@yahoo.com.br>
2017-12-08 00:45:40 -0200

    Criaçãodos Modelos e suas Relaçoes
    - Criação do Modelo Order
    - Criação do Modelo Kart
    - Criação do Modelo Product

--- Scripts/mvc/models.py
+++ Scripts/mvc/models.py
@@ -12,3 +12,21 @@ class Usuario(models.Model):
 	
 	def __str__(self):
 		return self.usuario	
+
+class Products(models.Model):
+	nm_produto = models.CharField(max_length=255)
+	valor_produto =  models.DecimalField(decimal_places=2,max_digits=7)
+	photo = models.ImageField(upload_to='products')
+
+class Kart(models.Model):
+	produto = models.ForeignKey('Products', on_delete=models.CASCADE)
+	qtd = models.IntegerField(default=0)
+	valor_produto =  models.DecimalField(decimal_places=2,max_digits=7)
+	dt_cadastro = models.DateField(blank=True)
+	dt_termino = models.DateField(null=True, blank=True)
+
+class Order(models.Model):
+	kart = models.ForeignKey('Kart', on_delete=models.CASCADE)
+	dt_cadastro = models.DateField(blank=True)
+	dt_termino = models.DateField(null=True, blank=True)
+
